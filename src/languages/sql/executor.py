271d4c20fb6ac3595c2cb5478115e0304889b7c4
szelee <szelee@temasek.com.sg>
2025-04-20 10:41:50 +0800

    Use st.connection to handle sql connection

--- src/languages/sql/executor.py
+++ src/languages/sql/executor.py
@@ -16,13 +16,7 @@ EXERCISE_DIR = Path(__file__).resolve().parent / "exercises"
 SQL_FILE_EXT = ".sql"
 DROP_SCHEMA_PUBLIC = "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 
-
-def get_engine() -> sa.Engine:
-    engine_url = st.secrets["DATABASE_URL"]
-    return sa.create_engine(url=engine_url, pool_pre_ping=True)
-
-
-engine = get_engine()
+conn = st.connection(name="sql", type="sql", autocommit=True, ttl=3600)
 
 
 def execute_query(query: str):
@@ -30,29 +24,30 @@ def execute_query(query: str):
     query = f"{DROP_SCHEMA_PUBLIC}\n\n{query}"
 
     try:
-        with engine.begin() as conn:
-            result = conn.execute(sa.text(query))
-
-            if result.returns_rows:
-                df = pd.DataFrame(result.fetchall(), columns=list(result.keys()))
-                st.dataframe(df, hide_index=True, use_container_width=True)
-            else:
+        with conn.session as session:
+            result = session.execute(sa.text(query))
+            rows = result.fetchall()
+            st.dataframe(rows, hide_index=True, use_container_width=True)
+    except sa.exc.ResourceClosedError:
+        # Handle the case where no rows are returned (e.g., for DDL statements)
                 st.success("Query executed successfully!")
-
     except Exception as e:
-        st.error(f"Error: {e}")
+        st.error(f"Error: {type(e)}")
 
 
 def list_table_names() -> list[str]:
     query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';"
 
+    table_names = []
     try:
-        with engine.begin() as conn:
-            result = conn.execute(sa.text(query))
-            return [row[0] for row in result.fetchall()]
+        with conn.session as session:
+            result = session.execute(sa.text(query))
+            rows = result.fetchall()
+            table_names = [row[0] for row in rows]
     except Exception as e:
         st.error(f"Error fetching tables: {e}")
-        return []
+
+    return table_names
 
 
 def show_tables(table_names: list[str]):
@@ -65,9 +60,12 @@ def show_tables(table_names: list[str]):
         with table_tab:
             query = f"SELECT * FROM {table_name};"
             try:
-                with engine.begin() as conn:
-                    df = pd.read_sql_query(query, con=conn)
-                st.dataframe(df, use_container_width=True, hide_index=True, height=387)
+                with conn.session as session:
+                    result = session.execute(sa.text(query))
+                    rows = result.fetchall()
+                    st.dataframe(
+                        rows, use_container_width=True, hide_index=True, height=387
+                    )
             except Exception as e:
                 st.error(f"Error fetching table {table_name}: {e}")
 
