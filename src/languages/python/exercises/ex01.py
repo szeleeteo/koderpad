09c8a993c37a2c410d0fe4738b5fac70e9fcf3e8
szelee <szelee@temasek.com.sg>
2025-10-20 10:10:03 +0800

    Refactor exercise descriptions for clarity and consistency

--- src/languages/python/exercises/ex01.py
+++ src/languages/python/exercises/ex01.py
@@ -1,16 +1,16 @@
 """
 You are given a list of students, where each student has a name, age and a list of grades.
 
-Write one Python function that accepts the list and returns new list with the following criteria:
-
-- Include only students who are 18 or older.
-- Include a new key "average_grade" that contains the average of their grades.
-- Exclude the original "age" and "grades" key from the returned list.
+Write one Python function the students list and returns a new list of dictionaries.
+Each dictionary in the new list should contain:
+- student's name and average grade
+- exclude age and grades
+- include only students who are 18 or older
 """
 
 class_students = [
     {"name": "Alice", "age": 17, "grades": [88, 92, 95]},
-    {"name": "Bob", "age": 20, "grades": [75, 85, 90]},
+    {"name": "Bob", "age": 20, "grades": [72, 86, 91]},
     {"name": "Charlie", "age": 18, "grades": [80, 85, 90]},
-    {"name": "Diana", "age": 22, "grades": [85, 88, 90]},
+    {"name": "Diana", "age": 22, "grades": [85, 53, 90]},
 ]
--- src/languages/python/exercises/ex02.py
+++ src/languages/python/exercises/ex02.py
@@ -1,25 +1,16 @@
-"""
-Study the code below. What will be the output of this code?
-If the next two commented lines are uncommented, what will be the output?
-Repeat for the last two commented lines.
-"""
-
-
 def double_and_append_to_list(element, elements=[]):
-    """
-    Double the value of an input.
-    Append the new value to a list provided; if no list is provided, use a new empty list.
-    Return the list.
-    """
     elements.append(element * 2)
     return elements
 
 
+# what will be the output of print statement?
 first_list = double_and_append_to_list(3, [2, 4])
 print(first_list)
 
+# what will be the output of print statement?
 # second_list = double_and_append_to_list(12)
 # print(second_list)
 
-# third_list = double_and_append_to_list("X")
+# what will be the output of print statement?
+# third_list = double_and_append_to_list("G")
 # print(third_list)
